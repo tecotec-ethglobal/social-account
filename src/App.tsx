bb7a51891132804124f848f04fb28c285fd66502
teco-mishima <mishima.sho@tecotec.co.jp>
2023-04-15 01:34:53 +0900

    fix2

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import "./App.css";
 
 import { SafeAuthKit, Web3AuthAdapter } from "@safe-global/auth-kit";
-import { Web3AuthOptions } from "@web3auth/modal";
+import { Web3Auth, Web3AuthOptions } from "@web3auth/modal";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
 
 class App extends Component {
     async componentDidMount(): Promise<void> {
@@ -13,7 +14,7 @@ class App extends Component {
             web3AuthNetwork: "testnet",
             chainConfig: {
                 chainNamespace: "eip155",
-                chainId: "11155111",
+                chainId: "0xAA36A7",
             },
         };
 
@@ -22,7 +23,17 @@ class App extends Component {
         // https://web3auth.io/docs/sdk/web/modal/whitelabel#whitelabeling-while-modal-initialization
         const openloginAdapter = new OpenloginAdapter({
             adapterSettings: {
+                clientId:
+                    "BM0Bh_56N1efD3O7Mqw0fg2hOSI3zhS-nv_5xsILiJP7BvoiJ7Rs96JOCc1paXlYd_1AIhFviTduLs4edDGKS0E",
+                network: "testnet", // Optional - Provide only if you haven't provided it in the Web3Auth Instantiation Code
                 uxMode: "popup",
+                whiteLabel: {
+                    name: "Your app Name",
+                    logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
+                    logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
+                    defaultLanguage: "en",
+                    dark: true, // whether to enable dark mode. defaultValue: false
+                },
             },
         });
 
@@ -33,13 +44,14 @@ class App extends Component {
             modalConfig
         );
 
-        console.log(web3AuthAdapter);
-        console.log(web3AuthAdapter);
-
         // Create an instance of the SafeAuthKit using the adapter and the SafeAuthConfig allowed options
-        const safeAuthKit = await SafeAuthKit.init(web3AuthAdapter, {});
+        const safeAuthKit = await SafeAuthKit.init(web3AuthAdapter);
 
         await safeAuthKit.signIn();
+
+        safeAuthKit.getProvider();
+
+        console.log(safeAuthKit.getProvider());
     }
 
     render() {
