0b04701a33a530a0fd92c94ef9d621f0cf9091ef
Kei Kamigama <kamigama.kei@tecotec.co.jp>
2023-04-16 02:59:56 +0900

    Added icon and copy feature to Address

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import { Text } from "evergreen-ui";
+import { Text, ClipboardIcon } from "evergreen-ui";
 
 type AddressType = {
   style?: { [key: string]: string | undefined };
   address?: string;
 };
+
+const styles = {
+  icon: {
+    cursor: "pointer",
+    verticalAlign: "baseline",
+  },
+};
+
 function Address({ address, style }: AddressType) {
   if (!address) return null;
 
   const abbrevAddress = `${address.slice(0, 6)}...${address.slice(-4)}`;
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(address);
+  };
 
   // return <Text style={style}>{abbrevAddress}</Text>;
-  return <>{abbrevAddress}</>;
+  return (
+    <span>
+      <ClipboardIcon style={styles.icon} onClick={copyToClipboard} /> {abbrevAddress}
+    </span>
+  );
 }
 
 export default Address;
