18ee36415bd43e8dcb16ba8f63eddd566c5ddeb9
Kei Kamigama <kamigama.kei@tecotec.co.jp>
2023-04-15 22:28:40 +0900

    AccountCreation logic

--- src/components/AccountCreation.tsx
+++ src/components/AccountCreation.tsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import Safe, { SafeFactory } from "@safe-global/protocol-kit";
 import { useNavigate } from "react-router-dom";
 
+import { AppContext } from "./AppContext";
+import OwnerList from "./OwnerList";
+
 function AccountCreation() {
+  console.log("update!");
   const navigate = useNavigate();
-  const deployContract = () => {
-    // TODO: deploy Safe account from form data
-    // navigate('/contract')
+  const { ethAdapter, safeAuthKit, setContractAddress } = useContext(AppContext);
+  const [owners, setOwners] = useState([safeAuthKit.safeAuthData?.eoa]);
+
+  const deployContract = async () => {
+    const safeFactory = await SafeFactory.create({ ethAdapter });
+    const safeSdk: Safe = await safeFactory.deploySafe({
+      safeAccountConfig: {
+        threshold: 1,
+        owners,
+      },
+    }); //5
+    const address = safeSdk.getAddress();
+    setContractAddress(address);
+    navigate(`/contract${address}`);
   };
 
-  // TODO: create form
   return (
     <main>
       <h1>Create your account</h1>
 
-      <div>
-        <h3>Owners</h3>
-      </div>
-      <div>
-        <h3>Threshold</h3>
-      </div>
+      <OwnerList owners={owners} updateOwners={setOwners} />
+
+      <button onClick={deployContract}>Deploy Contract</button>
     </main>
   );
 }
