1a7e8d7fa26cfef5691af0f0e66ab318044dd2ed
Kei Kamigama <kamigama.kei@tecotec.co.jp>
2023-04-15 17:02:16 +0900

    Added more fields to AppContext

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -5,6 +5,13 @@ type AppContextType = {
   setSigner: (signer: any) => void;
   contractAddress?: any;
   setContractAddress: (contractAddress: any) => void;
+  safeAuthData?: any;
+  isLoggedIn: () => boolean;
+  setSafeAuthData: (safeAuthData: any) => void;
+  safeAuthKit?: any;
+  setSafeAuthKit: (safeAuthKit: any) => void;
+  ethAdapter?: any;
+  setEthAdapter: (ethAdapter: any) => void;
 };
 type AppContextProviderProp = {
   children: JSX.Element;
@@ -13,14 +20,36 @@ type AppContextProviderProp = {
 const AppContext = createContext<AppContextType>({
   setSigner: () => {},
   setContractAddress: () => {},
+  isLoggedIn: () => false,
+  setSafeAuthData: () => {},
+  setSafeAuthKit: () => {},
+  setEthAdapter: () => {},
 });
 
 function AppContextProvider({ children }: AppContextProviderProp) {
   const [signer, setSigner] = useState(undefined);
   const [contractAddress, setContractAddress] = useState(undefined);
+  const [safeAuthData, setSafeAuthData] = useState(undefined);
+  const [safeAuthKit, setSafeAuthKit] = useState(undefined);
+  const [ethAdapter, setEthAdapter] = useState(undefined);
+  const isLoggedIn = () => !!safeAuthData;
 
   return (
-    <AppContext.Provider value={{ signer, setSigner, contractAddress, setContractAddress }}>
+    <AppContext.Provider
+      value={{
+        signer,
+        setSigner,
+        contractAddress,
+        setContractAddress,
+        safeAuthData,
+        isLoggedIn,
+        setSafeAuthData,
+        safeAuthKit,
+        setSafeAuthKit,
+        ethAdapter,
+        setEthAdapter,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
