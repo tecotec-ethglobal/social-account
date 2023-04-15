e703cf6d5b104642b032540779711426ad8b1cd2
teco-mishima <mishima.sho@tecotec.co.jp>
2023-04-16 06:13:54 +0900

    update index.css / Safe Account

--- src/components/SafeAccount.tsx
+++ src/components/SafeAccount.tsx
@@ -15,6 +15,25 @@ import SafeApiKit, {
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { AppContext } from "./AppContext";
 import Address from "./Address";
+import { ArrowRightIcon, Table, UserIcon } from "evergreen-ui";
+
+const styles = {
+  address: {
+    fontSize: "1.2rem",
+  },
+  balance: {
+    padding: "20px 0",
+  },
+  buttonGroup: {
+    padding: "10px 0 30px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  buttonIcon: {
+    padding: "0 18px",
+    cursor: "pointer",
+  },
+};
 
 function SafeAccount() {
   const { safeAddress } = useParams();
@@ -53,14 +72,87 @@ function SafeAccount() {
   }, []);
 
   return (
-    <div className="safeAccount">
-      <div>
+    <main>
+      <h1>Account</h1>
+      <div style={styles.address}>
         <Address address={safeAddress!} />
       </div>
-      <div>{balance} GoerliETH</div>
-      <button onClick={() => navigate(`/transfer/${safeAddress}`)}>Transfer</button>
-      <button onClick={() => navigate(`/owners/${safeAddress}`)}>Owners</button>
-      <ul>
+      <div style={styles.balance}>Balance: {balance} GoerliETH</div>
+      <div style={styles.buttonGroup}>
+        <div style={styles.buttonIcon} onClick={() => navigate(`/transfer/${safeAddress}`)}>
+          <ArrowRightIcon size={40} />
+          <div>Transfer</div>
+        </div>
+        <div style={styles.buttonIcon} onClick={() => navigate(`/owners/${safeAddress}`)}>
+          <UserIcon size={40} />
+          <div>Owners</div>
+        </div>
+      </div>
+      <Table minWidth={"380px"}>
+        <Table.VirtualBody minHeight={400}>
+          {transactions.map((t) => {
+            let txHash;
+            let kind;
+            let direction;
+            let address;
+            let amount;
+            let date = t.executionDate;
+            if (t.txType === "ETHEREUM_TRANSACTION") {
+              txHash = t.txHash;
+              let _amount = ethers.utils.formatEther(t.transfers[0].value);
+              if (t.to === safeAddress) {
+                kind = "Receive";
+                direction = "from";
+                address = t.from;
+                amount = `+ ${_amount}`;
+              } else {
+                kind = "Send";
+                direction = "to";
+                address = t.to;
+                amount = `- ${_amount}`;
+              }
+            } else if (t.txType === "MULTISIG_TRANSACTION") {
+              txHash = t.safeTxHash;
+              let _amount = ethers.utils.formatEther(t.value);
+              if (t.data != null) {
+                kind = "Contract method";
+                amount = 0;
+                if (t.to === safeAddress) {
+                  direction = "from";
+                  address = t.executor;
+                } else {
+                  direction = "to";
+                  address = t.to;
+                }
+              } else if (t.to === safeAddress) {
+                kind = "Receive";
+                direction = "from";
+                address = t.executor;
+                amount = `+ ${_amount}`;
+              } else {
+                kind = "Send";
+                direction = "to";
+                address = t.to;
+                amount = `- ${_amount}`;
+              }
+            }
+            return (
+              <Table.Row key={txHash}>
+                <Table.TextCell maxWidth={130}>
+                  <b>{kind}</b>
+                  <br />
+                  {new Date(date).toDateString()}
+                </Table.TextCell>
+                <Table.TextCell>
+                  {direction}: <Address address={address} />
+                </Table.TextCell>
+                <Table.TextCell maxWidth={100}>{amount}</Table.TextCell>
+              </Table.Row>
+            );
+          })}
+        </Table.VirtualBody>
+      </Table>
+      {/* <ul>
         {transactions.map((t) => {
           if (t.txType === "ETHEREUM_TRANSACTION") {
             if (t.to === safeAddress) {
@@ -96,8 +188,8 @@ function SafeAccount() {
             }
           }
         })}
-      </ul>
-    </div>
+      </ul> */}
+    </main>
   );
 }
 
